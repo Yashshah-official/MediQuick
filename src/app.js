b69bc2b23ff751fed8efef2cd4e689bcec6174cf
yash shah <139542346+Yashshah-official@users.noreply.github.com>
2024-12-03 11:13:48 +0530

    Basic backend code

---
+++ src/app.js
@@ -0,0 +1,49 @@
+import  express from 'express';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import connectDB from './db/index.js';
+import dotenv from "dotenv";
+import methodOverride from 'method-override'
+
+
+
+dotenv.config({
+    path: './.env'
+  })
+
+
+
+const app = express();
+const port = 5000;
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, "../public")))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.use(methodOverride('_method'));
+
+app.use(express.static(path.join(__dirname, "/public")))
+
+
+app.get('/', (req,res) => {
+    res.render('index.ejs');
+  })
+
+connectDB()
+.then(() => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+  } catch (error) {
+      console.log("Error : ", error);
+  }
+})
